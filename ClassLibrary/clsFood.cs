2d305f4ea310de9be038bd70631d90c0137a443d
VijayModhwadia <p13239500@my365.dmu.ac.uk>
2017-02-16 16:11:44 +0000

    I have added two classes on the class library

---
+++ ClassLibrary/clsFood.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Class_Library
+{
+    public class clsFood
+    {
+        //private data member for the FoodID property
+        private Int32 foodId;
+        //private data member for the FoodID property
+        private string foodName;
+        //private data member for the CompanyEmailAddress property
+        private string foodPrice;
+
+        public string Food { get; set; }
+        public int FoodID { get; set; }
+        public string FoodName { get; set; }
+        public string FoodPrice { get; set; }
+        public string Quantity { get; set; }
+
+        public bool Valid(string SomeFood)
+        {
+            //if the name of the food is not blank
+            if (SomeFood != "")
+            {
+                //return true
+                return true;
+            }
+            //otherwise return false
+            {
+                //return false
+                return false;
+            }
+        
+        }
+    }
+}
---
+++ ClassLibrary/clsFoodSupplier.cs
@@ -0,0 +1,14 @@
+﻿namespace Class_Library
+{
+    public class clsFoodSupplier
+    {
+        public clsFoodSupplier()
+        {
+        }
+
+        public string FoodSupplier { get; set; }
+        public string FoodSupplierAddress { get; set; }
+        public int FoodSupplierID { get; set; }
+        public string FoodSupplierName { get; set; }
+    }
+}
