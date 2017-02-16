4c07c93bea811b3514fb46a10a6b5f72aa657e7d
VijayModhwadia <p13239500@my365.dmu.ac.uk>
2017-02-16 16:09:30 +0000

    I have added two classes in this test framework

---
+++ TestFrameWork/tstFood.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_Library;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstFood
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the we want to create
+            clsFood AFood = new clsFood();
+            //test to see if this works
+            Assert.IsNotNull(AFood);
+        }
+
+        [TestMethod]
+        public void FoodPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFood AFood = new clsFood();
+            //create soem test data to assign to the property
+            string SomeFood = "Steak";
+            //assign the data to the property
+            AFood.Food = SomeFood;
+            //test to see that the two values are the same
+            Assert.AreEqual(AFood.Food, SomeFood);
+        }
+
+        [TestMethod]
+        public void FoodIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFood AFood = new clsFood();
+            //create some test data to assign to the property
+            Int32 TestData = 21;
+            //assign the data to the property
+            AFood.FoodID = TestData;
+            //test to see the two values are the same
+            Assert.AreEqual(AFood.FoodID, TestData);
+        }
+
+        [TestMethod]
+        public void FoodNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFood AFood = new clsFood();
+            //create some test data to assign to the property
+            string SomeFood = "Steak";
+            //assign the data to the property
+            AFood.FoodName = SomeFood;
+            //test to see that the two values are the same
+            Assert.AreEqual(AFood.FoodName, SomeFood);
+        }
+
+        [TestMethod]
+        public void FoodPricePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFood AFood = new clsFood();
+            //create some test data to assign to the property
+            string SomeFood = "Steak";
+            //assign the data to the property
+            AFood.FoodPrice = SomeFood;
+            //test to see that the two values are the same
+            Assert.AreEqual(AFood.FoodPrice, SomeFood);
+        }
+
+        [TestMethod]
+        public void FoodQuantityPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFood AFood = new clsFood();
+            //create some test data to assign to the property
+            string SomeFood = "Steak";
+            //assign the data to the property
+            AFood.Quantity = SomeFood;
+            //test to see that the two values are the same
+            Assert.AreEqual(AFood.Quantity, SomeFood);
+        }
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsFood AFood = new clsFood();
+            //boolean variable to store the result of the validation
+            Boolean OK = false;
+            //create some test data to asisgn to the property
+            string SomeFood = "Steak";
+            //invoke the method
+            OK = AFood.Valid(SomeFood);
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+    }
+}
---
+++ TestFrameWork/tstFoodSupplier.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_Library;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstFoodSupplier
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the we want to create
+            clsFoodSupplier AFoodSupplier = new clsFoodSupplier();
+            //test to see if this works
+            Assert.IsNotNull(AFoodSupplier);
+        }
+
+        [TestMethod]
+        public void FoodSupplierPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFoodSupplier AFoodSupplier = new clsFoodSupplier();
+            //create soem test data to assign to the property
+            string SomeFoodSupplier = "Steak";
+            //assign the data to the property
+            AFoodSupplier.FoodSupplier = SomeFoodSupplier;
+            //test to see that the two values are the same
+            Assert.AreEqual(AFoodSupplier.FoodSupplier, SomeFoodSupplier);
+        }
+
+        [TestMethod]
+        public void FoodSupplierIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFoodSupplier AFoodSupplier = new clsFoodSupplier();
+            //create some test data to assign to the property
+            Int32 TestData = 21;
+            //assign the data to the property
+            AFoodSupplier.FoodSupplierID = TestData;
+            //test to see the two values are the same
+            Assert.AreEqual(AFoodSupplier.FoodSupplierID, TestData);
+        }
+
+        [TestMethod]
+        public void FoodSupplierNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFoodSupplier AFoodSupplier = new clsFoodSupplier();
+            //create some test data to assign to the property
+            string SomeFoodSupplier = "Walkers";
+            //assign the data to the property
+            AFoodSupplier.FoodSupplierName = SomeFoodSupplier;
+            //test to see that the two values are the same
+            Assert.AreEqual(AFoodSupplier.FoodSupplierName, SomeFoodSupplier);
+        }
+
+        public void FoodSupplierAddressPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFoodSupplier AFoodSupplier = new clsFoodSupplier();
+            //create some test data to assign to the property
+            string SomeFoodSupplier = "Walkers";
+            //assign the data to the property
+            AFoodSupplier.FoodSupplierAddress = SomeFoodSupplier;
+            //test to see that the two values are the same
+            Assert.AreEqual(AFoodSupplier.FoodSupplierAddress, SomeFoodSupplier);
+        }
+    }
+}
