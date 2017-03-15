826c31f04c8bbc43121ed0d780d56d869fff5c1a
Vijay Modhwadia <vijaymodhwadia@hotmail.co.uk>
2017-03-15 18:23:16 +0000

    I have made a new class and added some many properties and fields in the class.

---
+++ ClassLibrary/clsFoodPayment.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary
+{
+    public class clsFoodPayment
+    {
+        public string FoodPayment { get; set; }
+        public string FoodPaymentCardNumber { get; set; }
+        public int FoodPaymentID { get; set; }
+    }
+}
---
+++ TestFrameWork/tstFoodPayment.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace TestFrameWork
+{
+    [TestClass]
+    public class tstFoodPayment
+    {
+        [TestMethod]
+        public void FoodPaymentInstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsFoodPayment AFoodPayment = new clsFoodPayment();
+            //test to see if the data works
+            Assert.IsNotNull(AFoodPayment);
+        }
+
+        [TestMethod]
+        public void FoodPaymentPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFoodPayment AFoodPayment = new clsFoodPayment();
+            //create some test data to assign to the property
+            string SomeFoodPayment = "Ten Pounds";
+            AFoodPayment.FoodPayment = SomeFoodPayment;
+            //test to see if the two values are the same
+            Assert.AreEqual(AFoodPayment.FoodPayment, SomeFoodPayment);
+        }
+
+        [TestMethod]
+        public void FoodPaymentIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFoodPayment AFoodPayment = new clsFoodPayment();
+            //create some test data to assign to the property
+            Int32 TestData = 21;
+            //assign the data to the property
+            AFoodPayment.FoodPaymentID = TestData;
+            //test to see the two values are the same
+            Assert.AreEqual(AFoodPayment.FoodPaymentID, TestData);
+        }
+
+        [TestMethod]
+        public void FoodPaymentCardNumberPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsFoodPayment AFoodPayment = new clsFoodPayment();
+            //create some test data to assign to the property
+            string SomeFoodPayment = "5151516561356565";
+            AFoodPayment.FoodPaymentCardNumber = SomeFoodPayment;
+            //test to see if the two values are the same
+            Assert.AreEqual(AFoodPayment.FoodPaymentCardNumber, SomeFoodPayment);
+        }
+
+
+    }
+}
