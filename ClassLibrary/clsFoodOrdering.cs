72b85a894cc1b22f12ded132415cf4ffa5f798f7
VijayModhwadia <vijaymodhwadia@hotmail.co.uk>
2017-05-11 13:22:10 +0100

    I have made more test classes and more property in test framework.

--- ClassLibrary/clsFoodOrdering.cs
+++ ClassLibrary/clsFoodOrdering.cs
@@ -1,6 +1,35 @@
-﻿namespace ClassLibrary
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Class_Library
 {
     public class clsFoodOrdering
     {
+        //private data member for the FoodID property
+        private Int32 foodOrderingId;
+        //private data member for the FoodID property
+        private string restaurantID;
+
+            public int FoodOrderingID { get; set; }
+            public int RestaurantID { get; set; }
+
+        public bool Valid(string SomeFoodOrdering)
+        {
+            //if the name of the food is not blank
+            if (SomeFoodOrdering != "")
+            {
+                //return true
+                return true;
+            }
+            else
+            //otherwise return false
+            {
+                //return false
+                return false;
+            }
+
+        }
     }
 }
